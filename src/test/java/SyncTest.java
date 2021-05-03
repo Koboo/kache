9fccda14c9bbf33f68564fd44db1df24da4f485d
Koboo <14143908+Koboo@users.noreply.github.com>
2021-05-03 16:06:43 +0200

    Create test-unit SyncTest

---
+++ src/test/java/SyncTest.java
@@ -0,0 +1,46 @@
+import eu.koboo.endpoint.core.protocols.ReceiveEvent;
+import eu.koboo.endpoint.core.protocols.natives.NativeReceiveEvent;
+import eu.koboo.endpoint.core.protocols.serializable.SerializableReceiveEvent;
+import eu.koboo.event.CallableEvent;
+import eu.koboo.event.listener.EventListener;
+import eu.koboo.netsync.NetSyncClient;
+import eu.koboo.netsync.NetSyncServer;
+import eu.koboo.netsync.operation.SyncModel;
+import eu.koboo.netsync.operation.SyncType;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SyncTest {
+
+    static NetSyncServer server;
+    static NetSyncClient client;
+
+    @BeforeClass
+    public static void before() {
+        server = new NetSyncServer();
+        Assert.assertTrue(server.start());
+        System.out.println("Server started!");
+        client = new NetSyncClient("localhost", 2525);
+        Assert.assertTrue(client.start());
+        System.out.println("Client started!");
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        SyncModel syncModel = new SyncModel();
+        syncModel.put("Geht das wirklich", "ja es geht!");
+        syncModel.put("test2", System.currentTimeMillis());
+        syncModel.put("isTrue", true);
+        client.syncModel(syncModel, SyncType.UPDATE);
+    }
+
+    @AfterClass
+    public static void after() {
+        if(server != null)
+            server.stop();
+        if(client != null)
+            client.stop();
+    }
+}
